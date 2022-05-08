f001ea2f731463b10f333960f0ca46d904ea8f02
Hansem Ro <hansemro@outlook.com>
2022-05-08 03:09:27 -0700

    Add src/pwm.v solution with extra credit
    Extra credit involves parametrizing internal counter and inverting
    output if parameter INVERTED_OUTPUT is set to 1.

--- src/pwm.v
+++ src/pwm.v
@@ -4,7 +4,25 @@ module pwm (
     input wire clk,
     input wire reset,
     output wire out,
-    input wire [7:0] level
+    input wire [LEVEL_WIDTH-1:0] level
     );
+
+    parameter LEVEL_WIDTH = 8;
+    parameter INVERTED_OUTPUT = 1'b0;
+
+    reg [LEVEL_WIDTH-1:0] counter;
+
+    always @(posedge clk)
+        if (reset)
+            counter <= 1'b0;
+        else
+            counter <= counter + 1'b1;
+
+    generate
+        if (INVERTED_OUTPUT)
+            assign out = reset ? 1'b1 : counter >= level;
+        else
+            assign out = reset ? 1'b0 : counter < level;
+    endgenerate
 
 endmodule

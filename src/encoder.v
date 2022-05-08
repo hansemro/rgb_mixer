b23b1d60844a6e817e7abd898a67e6eca86930b2
Hansem Ro <hansemro@outlook.com>
2022-05-08 03:29:52 -0700

    src/encoder.v: Parametrize output width and increment value

--- src/encoder.v
+++ src/encoder.v
@@ -5,8 +5,11 @@ module encoder (
     input reset,
     input a,
     input b,
-    output reg [7:0] value
+    output reg [VALUE_WIDTH-1:0] value
 );
+
+    parameter VALUE_WIDTH = 8;
+    parameter INCREMENT = 1'b1;
 
     reg prev_a;
     reg prev_b;
@@ -15,14 +18,14 @@ module encoder (
         prev_a <= a;
         prev_b <= b;
         if (reset) begin
-            value <= 8'b0;
+            value <= {VALUE_WIDTH{1'b0}};
             prev_a <= 1'b0;
             prev_b <= 1'b0;
         end
         else if ({a,prev_a,b,prev_b} == 4'b1000 || {a,prev_a,b,prev_b} == 4'b0111)
-            value <= value + 1'b1;
+            value <= value + INCREMENT;
         else if ({a,prev_a,b,prev_b} == 4'b0010 || {a,prev_a,b,prev_b} == 4'b1101)
-            value <= value - 1'b1;
+            value <= value - INCREMENT;
     end
 
 endmodule

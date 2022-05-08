7a69bea3b7f3a4f501785fd14f8b6aa1f7c1b440
Hansem Ro <hansemro@outlook.com>
2022-05-08 04:14:16 -0700

    src/rgb_mixer.v: use generate statements for extra credit

--- src/rgb_mixer.v
+++ src/rgb_mixer.v
@@ -14,42 +14,50 @@ module rgb_mixer (
     output pwm2_out
 );
 
+    localparam NUM_CHANNELS = 3; // should not be changed
     localparam BOUNCE_NS = 250000;
     localparam CLOCK_PERIOD_NS = 35700;
     localparam ENC_INCREMENT = 1'b1;
     localparam PWM_LEVEL_WIDTH = 8;
     localparam PWM_INVERTED_OUTPUT = 1'b0;
 
-    wire deb0_a, deb0_b, deb1_a, deb1_b, deb2_a, deb2_b;
+    wire _enc_a [NUM_CHANNELS-1:0];
+    wire _enc_b [NUM_CHANNELS-1:0];
+    wire _deb_a [NUM_CHANNELS-1:0];
+    wire _deb_b [NUM_CHANNELS-1:0];
+    wire [PWM_LEVEL_WIDTH-1:0] _enc_out [NUM_CHANNELS-1:0];
     wire [PWM_LEVEL_WIDTH-1:0] enc0;
     wire [PWM_LEVEL_WIDTH-1:0] enc1;
     wire [PWM_LEVEL_WIDTH-1:0] enc2;
+    wire _pwm_out [NUM_CHANNELS-1:0];
 
-    debounce #(.BOUNCE_NS(BOUNCE_NS), .CLOCK_PERIOD_NS(CLOCK_PERIOD_NS))
-        debouncer_0_a (.clk, .reset, .button(enc0_a), .debounced(deb0_a));
-    debounce #(.BOUNCE_NS(BOUNCE_NS), .CLOCK_PERIOD_NS(CLOCK_PERIOD_NS))
-        debouncer_0_b (.clk, .reset, .button(enc0_b), .debounced(deb0_b));
-    debounce #(.BOUNCE_NS(BOUNCE_NS), .CLOCK_PERIOD_NS(CLOCK_PERIOD_NS))
-        debouncer_1_a (.clk, .reset, .button(enc1_a), .debounced(deb1_a));
-    debounce #(.BOUNCE_NS(BOUNCE_NS), .CLOCK_PERIOD_NS(CLOCK_PERIOD_NS))
-        debouncer_1_b (.clk, .reset, .button(enc1_b), .debounced(deb1_b));
-    debounce #(.BOUNCE_NS(BOUNCE_NS), .CLOCK_PERIOD_NS(CLOCK_PERIOD_NS))
-        debouncer_2_a (.clk, .reset, .button(enc2_a), .debounced(deb2_a));
-    debounce #(.BOUNCE_NS(BOUNCE_NS), .CLOCK_PERIOD_NS(CLOCK_PERIOD_NS))
-        debouncer_2_b (.clk, .reset, .button(enc2_b), .debounced(deb2_b));
+    assign _enc_a[0] = enc0_a;
+    assign _enc_a[1] = enc1_a;
+    assign _enc_a[2] = enc2_a;
+    assign _enc_b[0] = enc0_b;
+    assign _enc_b[1] = enc1_b;
+    assign _enc_b[2] = enc2_b;
 
-    encoder #(.VALUE_WIDTH(PWM_LEVEL_WIDTH), .INCREMENT(ENC_INCREMENT))
-        encoder_0 (.clk, .reset, .a(deb0_a), .b(deb0_b), .value(enc0));
-    encoder #(.VALUE_WIDTH(PWM_LEVEL_WIDTH), .INCREMENT(ENC_INCREMENT))
-        encoder_1 (.clk, .reset, .a(deb1_a), .b(deb1_b), .value(enc1));
-    encoder #(.VALUE_WIDTH(PWM_LEVEL_WIDTH), .INCREMENT(ENC_INCREMENT))
-        encoder_2 (.clk, .reset, .a(deb2_a), .b(deb2_b), .value(enc2));
+    assign enc0 = _enc_out[0];
+    assign enc1 = _enc_out[1];
+    assign enc2 = _enc_out[2];
 
-    pwm #(.LEVEL_WIDTH(PWM_LEVEL_WIDTH), .INVERTED_OUTPUT(PWM_INVERTED_OUTPUT))
-        pwm_0 (.clk, .reset, .out(pwm0_out), .level(enc0));
-    pwm #(.LEVEL_WIDTH(PWM_LEVEL_WIDTH), .INVERTED_OUTPUT(PWM_INVERTED_OUTPUT))
-        pwm_1 (.clk, .reset, .out(pwm1_out), .level(enc1));
-    pwm #(.LEVEL_WIDTH(PWM_LEVEL_WIDTH), .INVERTED_OUTPUT(PWM_INVERTED_OUTPUT))
-        pwm_2 (.clk, .reset, .out(pwm2_out), .level(enc2));
+    assign pwm0_out = _pwm_out[0];
+    assign pwm1_out = _pwm_out[1];
+    assign pwm2_out = _pwm_out[2];
+
+    genvar i;
+    generate
+        for (i = 0; i < 3; i = i + 1) begin : color_channel
+            debounce #(.BOUNCE_NS(BOUNCE_NS), .CLOCK_PERIOD_NS(CLOCK_PERIOD_NS))
+                _debouncer_a (.clk, .reset, .button(_enc_a[i]), .debounced(_deb_a[i]));
+            debounce #(.BOUNCE_NS(BOUNCE_NS), .CLOCK_PERIOD_NS(CLOCK_PERIOD_NS))
+                _debouncer_b (.clk, .reset, .button(_enc_b[i]), .debounced(_deb_b[i]));
+            encoder #(.VALUE_WIDTH(PWM_LEVEL_WIDTH), .INCREMENT(ENC_INCREMENT))
+                _encoder (.clk, .reset, .a(_deb_a[i]), .b(_deb_b[i]), .value(_enc_out[i]));
+            pwm #(.LEVEL_WIDTH(PWM_LEVEL_WIDTH), .INVERTED_OUTPUT(PWM_INVERTED_OUTPUT))
+                _pwm (.clk, .reset, .out(_pwm_out[i]), .level(_enc_out[i]));
+        end
+    endgenerate
 
 endmodule
